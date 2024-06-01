1624730027af4818ce983b05af3c8639545eaf89
saralotfy112 <sara26216562@gmail.com>
2024-06-01 22:23:32 +0300

    Add files via upload

---
+++ task5.js
@@ -0,0 +1,8 @@
+var num1 = prompt("ُEnter the first num");
+var num2 = prompt("enter the second num");
+
+var result = num1 * num2;
+    
+
+
+document.getElementById("result").innerHTML =   num1 + " × " + num2 + " = " + result;
